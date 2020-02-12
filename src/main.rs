aca56051f98a3f7f775551d7f6bce3532fd5ed76
Hermann Schwarz <tux1977@gmx.de>
2020-02-12 07:54:05 +0100

    Try to read remote JSON with curl::http

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Read};
 
+
+extern crate curl;
+extern crate serde_json;
+
+use serde_json::Value;
+use curl::easy;
+
+
 fn main() {
     let filename = "resources/DIE-DOK_IDs_alle.txt";
     // Open the file in read-only mode (ignoring errors).
@@ -24,12 +32,43 @@ fn main() {
 
     println!("");
 
-    print_to_stdout(&mut reader);
+  //  print_to_stdout(&mut reader);
+
+
+  read_json();
+}
+
+/*
+* try to read JSON with curl::http
+* https://docs.rs/curl/0.5.0/curl/
+* https://hermanradtke.com/2015/09/21/get-data-from-a-url-rust.html
+*/
+fn read_json() {
+
+    let url = "https://www.hautelook.com/api";
+    let resp = http::handle()
+        .get(url)
+        .exec()
+        .unwrap_or_else(|e| {
+            panic!("Failed to get {}; error is {}", url, e);
+        });
+
+    if resp.get_code() != 200 {
+        println!("Unable to handle HTTP response code {}", resp.get_code());
+        return;
+    }
+
+    let body = std::str::from_utf8(resp.get_body()).unwrap_or_else(|e| {
+        panic!("Failed to parse response from {}; error is {}", url, e);
+    });
+
 }
 
 
 
+/*
 fn print_to_stdout(mut input: &mut Read) {
     let mut stdout = io::stdout();
     io::copy(&mut input, &mut stdout);
 }
+*/
