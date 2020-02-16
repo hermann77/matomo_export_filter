08cc411ceec09d5121546d57d2453cc64eb74c7a
Hermann Schwarz <tux1977@gmx.de>
2020-02-16 20:17:55 +0100

    Working version with option -url (Matomo export URL)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Read};
 
-
 extern crate curl;
 extern crate serde_json;
+extern crate clap;
 
+use curl::http;
 use serde_json::Value;
-use curl::easy;
+use clap::{Arg,App};
 
 
 fn main() {
+
+    let matches = App::new("Matomo export filter")
+    .version("0.1.0")
+    .author("Hermann Schwarz")
+    .about("Parses matomo export and filters it to get only special statistics")
+    .arg(Arg::with_name("url")
+        .short("u")
+        .long("url")
+        .takes_value(true)
+        .help("url of the export"))
+    .get_matches();
+
+    let url = matches.value_of("url").unwrap();
+    println!("input URL: {}", url);
+
+
+//  read_ids();
+  read_json(url);
+}
+
+
+fn read_ids() {
     let filename = "resources/DIE-DOK_IDs_alle.txt";
     // Open the file in read-only mode (ignoring errors).
     let file = File::open(filename).unwrap();
@@ -26,26 +49,19 @@ fn main() {
         
         if index + 1 < line_number {
             print!("|");
-        }
-        
+        }   
     }
-
     println!("");
-
-  //  print_to_stdout(&mut reader);
-
-
-  read_json();
 }
+
 
 /*
 * try to read JSON with curl::http
 * https://docs.rs/curl/0.5.0/curl/
 * https://hermanradtke.com/2015/09/21/get-data-from-a-url-rust.html
 */
-fn read_json() {
+fn read_json(url : &str) {
 
-    let url = "https://www.hautelook.com/api";
     let resp = http::handle()
         .get(url)
         .exec()
@@ -62,13 +78,24 @@ fn read_json() {
         panic!("Failed to parse response from {}; error is {}", url, e);
     });
 
+    let json: Value = serde_json::from_str(body).unwrap_or_else(|e| {
+        panic!("Failed to parse json; error is {}", e);
+    });
+
+    let json_len = json.as_array().unwrap().len();
+    println!("JSON len: {}", json_len);
+
+    for i in 0..json_len {
+        let label = &json[i]["label"];
+        println!("Label: {}", label);
+
+        if label == "literatur" { // parse only in DOMAIN/literatur/*
+            let subtable = &json[i]["subtable"];
+            let subtable_size = subtable.as_array().unwrap().len();
+            println!("subtable size: {}", subtable_size); 
+            break;
+        }
+    }
+
 }
 
-
-
-/*
-fn print_to_stdout(mut input: &mut Read) {
-    let mut stdout = io::stdout();
-    io::copy(&mut input, &mut stdout);
-}
-*/
