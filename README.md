37aa785fbbf209c7c8d38d4ad55b380135a70ffb
Hermann Schwarz <tux1977@gmx.de>
2020-04-26 21:04:05 +0200

    You can output 'nb_hits' (pageviews) for hard coded URL <DOMAIN>/<dir1>/<dir2> and only if <dir2> contains a string hard coded in sources, e.g. DOMAIN/bookmark/<dir2> and <dir2> contains '32290'

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # matomo_export_filter
+Parses matomo JSON export file and filters it to get only special statistics.
+
+
+## Usage
+matomo_export_filter/target/debug/matomo_export_filter -u='<URL>'
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader, Read};
+// use std::fs::File;
+// use std::io::{self, BufRead, BufReader, Read};
 
 extern crate curl;
 extern crate serde_json;
@@ -15,7 +15,7 @@ fn main() {
     let matches = App::new("Matomo export filter")
     .version("0.1.0")
     .author("Hermann Schwarz")
-    .about("Parses matomo export and filters it to get only special statistics")
+    .about("Parses matomo JSON export file and filters it to get only special statistics")
     .arg(Arg::with_name("url")
         .short("u")
         .long("url")
@@ -38,7 +38,10 @@ fn main() {
   read_json(url);
 }
 
-
+/**
+ *  Reading a text file with IDs
+ */
+/*
 fn read_ids() -> std::vec::Vec<std::result::Result<std::string::String, std::io::Error>> {
     let filename = "resources/dok_ids.txt";
     // Open the file in read-only mode (ignoring errors).
@@ -47,7 +50,7 @@ fn read_ids() -> std::vec::Vec<std::result::Result<std::string::String, std::io:
     let ids: Vec<_> = reader.lines().collect();
     return ids;
 }
-
+*/
 
 /*
 * try to read JSON with curl::http
@@ -83,17 +86,31 @@ fn read_json(url : &str) {
         let label = &json[i]["label"];
         println!("Label: {}", label);
 
-        if label == "literatur" { // parse only in DOMAIN/literatur/*
+       // if label == "literatur" { // parse only in DOMAIN/literatur/*
+        if label == "bookmark" { // parse only in DOMAIN/bookmark/*
+
+            let bookmarks_nb_hits = &json[i]["nb_hits"];
+            println!("Hits in bookmarks: {}", bookmarks_nb_hits); 
+
             let subtable_array = &json[i]["subtable"];
             let subtable_size = subtable_array.as_array().unwrap().len();
             println!("subtable size: {}", subtable_size); 
 
             for j in 0..subtable_size {
                 let subtable_label = &subtable_array[j]["label"];
-                println!("subtable label: {}", subtable_label);
+                let subtable_label_string = subtable_label.to_string();
+
+                if subtable_label_string.contains("32290") { // parse only in DOMAIN/bookmark/32290/*
+                    println!("subtable label: {}", subtable_label);
+
+                    let hits = &subtable_array[j]["nb_hits"];
+                    println!("HITS in 32290: {}", hits);
+
+                    break;
+                }
             }
 
-            break;
+            break; // use break because we're finished (reached "/bookmarks")
         }
     }
 
