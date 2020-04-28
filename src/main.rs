a5d0dc23b1c18cb87835d0d62924f8f3649279b3
Hermann Schwarz <tux1977@gmx.de>
2020-04-28 10:23:19 +0200

    added 'prase_in_one_subtable()' to calculate statistics also for pre-filtered AND pre-selected (selected directory/subtable) exports

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 // use std::fs::File;
 // use std::io::{self, BufRead, BufReader, Read};
 
+extern crate clap;
 extern crate curl;
 extern crate serde_json;
-extern crate clap;
 
+use clap::{App, Arg};
 use curl::http;
 use serde_json::Value;
-use clap::{Arg,App};
-
 
 fn main() {
-
     let matches = App::new("Matomo export filter")
     .version("0.1.0")
     .author("Hermann Schwarz")
@@ -40,16 +38,15 @@ fn main() {
 
     println!("input URL: {}", url);
 
+    /*
+      let ids = read_ids();
+      for (index, id) in ids.into_iter().enumerate() {
+          let line = line.unwrap();
+          print!("FId={}", line);
+      }
+    */
 
-/*
-  let ids = read_ids();
-  for (index, id) in ids.into_iter().enumerate() {
-      let line = line.unwrap();
-      print!("FId={}", line);
-  }
-*/
-
-  read_json(url, dir1, filter);
+    read_json(url, dir1, filter);
 }
 
 /**
@@ -66,19 +63,15 @@ fn read_ids() -> std::vec::Vec<std::result::Result<std::string::String, std::io:
 }
 */
 
-/*
+/**
 * try to read JSON with curl::http
 * https://docs.rs/curl/0.5.0/curl/
 * https://hermanradtke.com/2015/09/21/get-data-from-a-url-rust.html
 */
-fn read_json(url : &str, dir1 : &str, filter : &str) {
-
-    let resp = http::handle()
-        .get(url)
-        .exec()
-        .unwrap_or_else(|e| {
-            panic!("Failed to get {}; error is {}", url, e);
-        });
+fn read_json(url: &str, dir1: &str, filter: &str) {
+    let resp = http::handle().get(url).exec().unwrap_or_else(|e| {
+        panic!("Failed to get {}; error is {}", url, e);
+    });
 
     if resp.get_code() != 200 {
         println!("Unable to handle HTTP response code {}", resp.get_code());
@@ -93,26 +86,60 @@ fn read_json(url : &str, dir1 : &str, filter : &str) {
         panic!("Failed to parse json; error is {}", e);
     });
 
+    // @TODO: check also id right Matomo API used (&filter_pattern AND &idSubtable MUST be SET)
+    if dir1.is_empty() {
+        // if matomo export includes only statistics for only one subdir/subtable (Matomo API 'idSubtable') e.g. DOMAIN/<DIR1>/*
+        // and additionally used a prefilter (Matomo API 'filter_pattern'):
+        // e.g. www.MATOMO.com/index.php?date=2017-04-07
+        //                      &expanded=1&filter_limit=-1
+        //                      &format=JSON
+        //                      &idSite=1
+        //                      &method=Actions.getPageUrls
+        //                      &module=API........
+        //                      &filter_pattern=loc
+        //                      &idSubtable=1236
+        prase_in_one_subtable(json, filter);
+    } else {
+        // matomo export includes entire statistics (all the subdirectories/subtables): for DOMAIN/<DIR1>/*, DOMAIN/<DIR2>/*, DOMAIN/<DIRn>/* etc.
+        // so we have to parse all the dubdirs to find the wanted dubtable
+        parse_in_all_subtables(json, dir1, filter);
+    }
+}
+
+
+/**
+ *  If matomo export includes only statistics for only one subdir/subtable (Matomo API 'idSubtable')
+ *    e.g. only for DOMAIN/<DIR1>/
+ *    and additionally used a prefilter (Matomo API 'filter_pattern'):
+ *    
+ *    e.g. www.MATOMO.com/index.php?date=2017-04-07
+ *                             &expanded=1&filter_limit=-1
+ *                             &format=JSON
+ *                             &idSite=1
+ *                             &method=Actions.getPageUrls
+ *                             &module=API........
+ *                             &filter_pattern=loc
+ *                             &idSubtable=1236
+ */
+fn parse_in_all_subtables(json: Value, dir1: &str, filter: &str) {
     let json_len = json.as_array().unwrap().len();
     println!("JSON len: {}", json_len);
     println!();
 
-
-
-
     for i in 0..json_len {
         let label = &json[i]["label"];
 
-        if label == dir1 { // parse only in DOMAIN/<DIR1>/* (set by command line option -d)
+        if label == dir1 {
+            // parse only in DOMAIN/<DIR1>/* (set by command line option -d)
 
             println!("Parsing directory DIR1: DOMAIN/{}", label);
 
             let bookmarks_nb_hits = &json[i]["nb_hits"];
-            println!("Hits in DOMAIN/{}: {}", dir1, bookmarks_nb_hits); 
+            println!("Hits in DOMAIN/{}: {}", dir1, bookmarks_nb_hits);
 
             let subtable_array = &json[i]["subtable"];
             let subtable_size = subtable_array.as_array().unwrap().len();
-            println!("Amount of sites (subtable size): {}", subtable_size); 
+            println!("Amount of sites (subtable size): {}", subtable_size);
 
             let mut hits_sum: i32 = 0;
 
@@ -120,11 +147,10 @@ fn read_json(url : &str, dir1 : &str, filter : &str) {
                 let subtable_label = &subtable_array[j]["label"];
                 let subtable_label_string = subtable_label.to_string();
 
-                if subtable_label_string.contains(filter) { // parse only in DOMAIN/bookmark/32290/ (if e.g. filter = 32290)
-                    
+                if subtable_label_string.contains(filter) {
+                    // parse only in DOMAIN/bookmark/32290/ (if e.g. filter = 32290)
                     let subtable_url = &subtable_array[j]["url"];
-                    println!("URL: {}", subtable_url);    
-                    
+                    println!("URL: {}", subtable_url);
                     println!("Script found: {}", subtable_label);
                     println!("Filtered by {}", filter);
 
@@ -132,7 +158,7 @@ fn read_json(url : &str, dir1 : &str, filter : &str) {
                     let hits_integer = hits_string.parse::<i32>().unwrap();
                     println!("HITS: {}", hits_integer);
                     hits_sum = hits_sum + hits_integer;
-                //    break; // don't break because we want to calculate hits sum for all the found (by filtering) scripts
+                    //    break; // don't break because we want to calculate hits sum for all the found (by filtering) scripts
                 }
             }
 
@@ -141,6 +167,38 @@ fn read_json(url : &str, dir1 : &str, filter : &str) {
             break; // use break because we're finished (reached "/bookmarks")
         }
     }
-
 }
 
+
+/**
+ *  If Matomo export includes entire statistics (all the subdirectories/subtables): 
+ *  for DOMAIN/<DIR1>/.., DOMAIN/<DIR2>/.., DOMAIN/<DIRn>/.. etc.
+ *  so we have to parse all the subdirs to find the wanted dubtable
+ */
+fn prase_in_one_subtable(json: Value, filter: &str) {
+    let json_len = json.as_array().unwrap().len();
+    println!("Number of matched sites (JSON len): {}", json_len);
+    println!();
+
+    let mut hits_sum: i32 = 0;
+
+    for i in 0..json_len {
+        let label = &json[i]["label"];
+        let label_string = &label.to_string();
+
+        if label_string.contains(filter) {
+
+            println!("Statistic for URL:{}", label);
+
+            let nb_hits = &json[i]["nb_hits"];
+            println!("Hits: {}", nb_hits);
+    
+            let hits_string = &nb_hits.to_string();
+            let hits_integer = hits_string.parse::<i32>().unwrap();
+           
+            hits_sum = hits_sum + hits_integer;
+        }
+
+        println!("Sum of HITS: {}", hits_sum);
+    }
+}
